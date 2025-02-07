76c60f41fdd050fdeab16f13c9523ced0cdd9715
Prince <120583874+prince-2003@users.noreply.github.com>
2025-02-07 12:52:48 +0530

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Sidebar from './components/Sidebar'
+import Hero from './hero'
 
 
 function App() {
@@ -8,10 +9,7 @@ function App() {
     <div className='flex '>
       <div className=" h-screen w-1/6 bg-[#1a1a1a] text-white p-6 flex flex-col"><Sidebar /></div>
       <div className='w-5/6 bg-gray-100 p-6'>
-        <h2 className="text-xl font-roboto">Main Content Area</h2>
-        <p className="mt-4">
-          This shows how the sidebar looks with main content.
-        </p>
+      <Hero/>
         </div>
      </div>
     
